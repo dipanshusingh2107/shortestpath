c73a6770858a4054803ec4b6ce803e55261866d8
Sushant Kumar <somi@Somis-MacBook-Air.local>
2020-08-16 02:22:11 +0530

    wrote the bellman ford code

--- shortest_path.cpp
+++ shortest_path.cpp
@@ -4,10 +4,27 @@
 using namespace std;
 
 
-void shortest_path(list<pair<int,int>> graph[],int source,int dest,int n)
+void shortest_path(list<pair<int,int>> graph[],int src,int dest,int n)
 {
     long long int distance[n+1]={9999999};  //distance of i node from source
-    distance[source]=0;   
+    distance[src]=0;
+    int parent[n+1];
+    parent[src]=-1;
+
+
+    for(int i=1;i<=n;i++)
+    {
+        for(auto j:graph[i])           // i->j.first   i links to j.first 
+        {
+            if(distance[i]+j.second<distance[j.first])  //j.second contains weight
+            {
+                distance[j.first]=distance[i]+j.second;  
+                parent[j.first]=i;
+            }
+        }
+    } 
+
+        cout<<"min weight: "<<distance[dest];
 
 }
 
@@ -28,8 +45,10 @@ int main()
         graph[u].push_back({v,w});
         graph[v].push_back({u,w});
     }
-
-
+    int src,dest;
+    cout<<"Enter source and dest ";
+    cin>>src>>dest;
+    shortest_path(graph,src,dest,n);
 
     return 0;
 }
